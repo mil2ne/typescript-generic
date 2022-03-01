20f253ffc88ba18ca8168f04bdd023519e60b8e1
mil2ne <show3070@gmail.com>
2022-03-02 07:54:23 +0900

    typescript-generic-7

---
+++ generic7.ts
@@ -0,0 +1,28 @@
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+const person: IPerson = {
+  name: 'Mark',
+  age: 39
+};
+
+
+function getProp<T, K extends keyof T>(
+  obj: T, 
+  key: K
+  ): T[K] {
+  return obj[key];
+}
+
+
+function setProp<T, K extends keyof T>(
+  obj: T, 
+  key: K, 
+  value: T[K]
+  ): void {
+  obj[key] = value;
+}
+
+setProp(person, 'name', "mark");
